b79fb380c37e0bf87132e39e211ef6f2b9f396c3
Ali khosronejad <khosronejadali344@gmail.com>
2024-12-01 14:12:04 +0330

    commit

--- src/components/ClientComments/ClientComments.jsx
+++ src/components/ClientComments/ClientComments.jsx
@@ -79,7 +79,7 @@ function CommentItem({clientName , jobTitle , photo , inView , ref}){
 
 
         <div>
-            <p className="text-center yekahbakhLight leading-loose text text-justify text-sm mt-5 lg:mt-0">
+            <p className=" yekahbakhLight leading-loose text text-justify text-sm mt-5 lg:mt-0">
             لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، 
             </p>
         </div>
